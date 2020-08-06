f3a364cfaa758bf044fb6495ef11480c04748e51
nirajdahal09@gmail.com <nirajdahal09@gmail.com>
2020-08-06 14:59:19 +0545

    Moved console helper class to a different file

---
+++ ExtensionMethodAndMethodOverloading/ConsoleHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ExtensionMethodAndMethodOverloading
+{
+    public static class ConsoleHelper
+    {
+        public static string RequestString(this string message)
+        {
+            string output;
+            do
+            {
+                Console.WriteLine(message);
+                output = Console.ReadLine();
+            } while (output.Length == 0);
+            return output;
+        }
+
+        public static int RequestInt (this string message)
+        {
+
+            return message.RequestInt(false);
+        }
+
+        public static int RequestInt(this string message, int minAge, int maxAge)
+        {
+
+            return message.RequestInt(true, minAge, maxAge);
+        }
+
+        private static int RequestInt(this string message, bool applyMinMax, int minAge=0, int maxAge=100)
+        {
+
+            bool isValid;
+            int output;
+            bool isInValidRange = true;
+
+            do
+            {
+                Console.WriteLine(message);
+                string userInput = Console.ReadLine();
+                isValid = int.TryParse(userInput, out int convertedInt);
+                output = convertedInt;
+
+
+                if(applyMinMax == true)
+                {
+                    isInValidRange = (output >= minAge && output <= maxAge);
+                    //if(output >= minAge && output <= maxAge)
+                    //{
+                    //    isInValidRange = true;
+                    //}
+                    //else
+                    //{
+                    //    isInValidRange = false;
+                    //}
+                }
+
+
+
+            } while (isValid == false || isInValidRange == false) ;
+
+            return output;
+        }
+    }
+}
--- ExtensionMethodAndMethodOverloading/Program.cs
+++ ExtensionMethodAndMethodOverloading/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Xml;
+﻿using System.Xml;
 using Library.Models;
 
 namespace ExtensionMethodAndMethodOverloading
@@ -14,67 +13,6 @@ namespace ExtensionMethodAndMethodOverloading
             //person.Age = "What is your Age".RequestInt();
 
             person.Age = "What is your Age".RequestInt(0, 120);
-        }
-    }
-
-    public static class ConsoleHelper
-    {
-        public static string RequestString(this string message)
-        {
-            string output;
-            do
-            {
-                Console.WriteLine(message);
-                output = Console.ReadLine();
-            } while (output.Length == 0);
-            return output;
-        }
-
-        public static int RequestInt (this string message)
-        {
-
-            return message.RequestInt(false);
-        }
-
-        public static int RequestInt(this string message, int minAge, int maxAge)
-        {
-
-            return message.RequestInt(true, minAge, maxAge);
-        }
-
-        public static int RequestInt(this string message, bool applyMinMax, int minAge=0, int maxAge=100)
-        {
-
-            bool isValid;
-            int output;
-            bool isInValidRange = true;
-
-            do
-            {
-                Console.WriteLine(message);
-                string userInput = Console.ReadLine();
-                isValid = int.TryParse(userInput, out int convertedInt);
-                output = convertedInt;
-
-
-                if(applyMinMax == true)
-                {
-                    isInValidRange = (output >= minAge && output <= maxAge);
-                    //if(output >= minAge && output <= maxAge)
-                    //{
-                    //    isInValidRange = true;
-                    //}
-                    //else
-                    //{
-                    //    isInValidRange = false;
-                    //}
-                }
-
-
-
-            } while (isValid == false || isInValidRange == false) ;
-
-            return output;
         }
     }
 }
